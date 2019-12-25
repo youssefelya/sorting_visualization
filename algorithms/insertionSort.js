0e2c69ebe3b3f0f81d95fc66ebca242ce7fb5cd2
youssefelya <youssefelyamani96@gmail.com>
2019-12-25 10:23:41 +0100

    fixing insertion

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -6,17 +6,22 @@
         arr[x].style.background = SELCETED_KEY_COLOR;
         key = arr[x].style.height;
         value = arr[x].innerText;
-        console.log(" value  ", value );
+
         await timer(ms);
         y = x - 1;
         while( y>=0 && parseInt(arr[y].style.height) > parseInt(key)){
-            let temp = arr[ y + 1];
-            arr[ y + 1].style.height = arr[y].style.height;
-            arr[ y + 1].innerText = arr[y].innerText;
-            arr[ y + 1].style.background = SELECTED_COLOR;
-            arr[y] = temp;
+            // let temp = arr[ y + 1];
+            // console.log(" before arr[y] ", arr[y].style.height);
+            // console.log(" before--> arr[y+1] ;", arr[y+1].style.height);
+            // arr[ y + 1].style.height = arr[y].style.height;
+            // arr[ y + 1].innerText = arr[y].innerText;
+            // arr[ y + 1].style.background = SELECTED_COLOR;
+            // arr[y] = temp;
+            // console.log(" after arr[y].innerText;", arr[y].innerText);
+            // console.log(" after--> arr[y+1] ;", arr[y+1].innerText);
+            swapi(arr, y+1, y, ms);
             await timer(ms);
-            arr[ y + 1].style.background = FINAL_COLOR;
+           // arr[ y + 1].style.background = FINAL_COLOR;
             y= y-1;
         }
         arr[y+1].style.height = key ;
